a2a2ad317a9a02be3819de2531dcd49b8cb2d2bb
Ramandeep Chamba <ramanchamba98@gmail.com>
2024-07-23 16:15:31 +0530

    Completed the project

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import monitor from "./imgs/monitor.jpg";
 import headphone from "./imgs/headphones.jpg";
 import microwave from "./imgs/microwave.jpg";
 import shoes from "./imgs/shoes.jpg";
+import { useState } from "react";
 
 const products = [
   {
@@ -30,60 +31,109 @@ const products = [
   },
 ];
 function App() {
+  const [cart, setCart] = useState([]);
+
+  function handleAddToCart(product) {
+    const itemIndex = cart.findIndex((item) => item.id === product.id);
+    // Check if product already in cart
+    if (itemIndex !== -1) {
+      // if already max quantity return
+      if (cart[itemIndex].quantity === 4) return;
+      // - yes
+      // -- update quantity
+      setCart((cart) =>
+        cart.map((item) =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        )
+      );
+    } else {
+      // - no
+      // -- convert product to a cart item
+      const item = { ...product, quantity: 1 };
+      // -- add to cart
+      setCart((cart) => [...cart, item]);
+    }
+  }
+
+  function handleChangeQuantity(id, quantity) {
+    // update quantity
+    setCart((cart) =>
+      cart.map((item) =>
+        item.id === id ? { ...item, quantity: quantity } : item
+      )
+    );
+  }
+
+  function handleDeleteFromCart(id) {
+    setCart((cart) => cart.filter((item) => item.id !== id));
+  }
+
   return (
     <div className="App">
-      <Nav />
-      <ProductList />
+      <Nav
+        cart={cart}
+        onChangeQuantity={handleChangeQuantity}
+        onDeleteItem={handleDeleteFromCart}
+      />
+      <ProductList onAddToCart={handleAddToCart} />
     </div>
   );
 }
 
-function Nav() {
+function Nav({ cart, onChangeQuantity, onDeleteItem }) {
+  const totalItems = cart.reduce((acc, item) => acc + item.quantity, 0);
   return (
     <nav className="nav">
       <h3>Amyzon</h3>
       <div className="cart__container">
         <span className="cart__toggler">
-          Cart <span>(2)</span>
+          Cart {cart.length > 0 && <span>({totalItems})</span>}
         </span>
-        <Cart />
+        <Cart
+          cart={cart}
+          totalItems={totalItems}
+          onChangeQuantity={onChangeQuantity}
+          onDeleteItem={onDeleteItem}
+        />
       </div>
     </nav>
   );
 }
 
-function Cart() {
-  const cartItems = [
-    {
-      id: 1,
-      title: "Monitor",
-      price: 10000,
-      image: monitor,
-      quantity: 1,
-    },
-    {
-      id: 2,
-      title: "Headphones",
-      price: 2000,
-      image: headphone,
-      quantity: 2,
-    },
-  ];
+function Cart({ cart, totalItems, onChangeQuantity, onDeleteItem }) {
+  const totalPrice = cart.reduce(
+    (acc, item) => acc + item.quantity * item.price,
+    0
+  );
 
   return (
     <div className="cart">
-      {/* Cart items */}
-      <ul className="list cart__list">
-        {cartItems.map((item) => (
-          <CartItem key={`cart-item-${item.id}`} item={item} />
-        ))}
-      </ul>
-      <div className="cart__total">SubTotal(2 items): Rs. 20000</div>
+      {cart.length ? (
+        <>
+          <ul className="list cart__list">
+            {cart.map((item) => (
+              <CartItem
+                key={`cart-item-${item.id}`}
+                item={item}
+                onChangeQuantity={onChangeQuantity}
+                onDeleteItem={onDeleteItem}
+              />
+            ))}
+          </ul>
+          <div className="cart__total">
+            SubTotal({totalItems} items): Rs. {totalPrice}
+          </div>
+        </>
+      ) : (
+        <p>No items</p>
+      )}
     </div>
   );
 }
 
-function CartItem({ item }) {
+function CartItem({ item, onChangeQuantity, onDeleteItem }) {
   return (
     <li className="item">
       <img src={item.image} alt={item.title} />
@@ -91,13 +141,16 @@ function CartItem({ item }) {
         <h4>{item.title}</h4>
         <div className="item__actions">
           <label>Qty</label>
-          <select defaultValue={item.quantity}>
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
+          <select
+            value={item.quantity}
+            onChange={(e) => onChangeQuantity(item.id, Number(e.target.value))}
+          >
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
           </select>
-          <button>Delete</button>
+          <button onClick={() => onDeleteItem(item.id)}>Delete</button>
         </div>
       </div>
       <div className="item__price">
@@ -107,25 +160,34 @@ function CartItem({ item }) {
   );
 }
 
-function ProductList() {
+function ProductList({ onAddToCart }) {
   const productsList = products;
   return (
     <ul className="list product__list">
       {productsList.map((product) => (
-        <Product product={product} key={`product-item-${product.id}`} />
+        <Product
+          product={product}
+          key={`product-item-${product.id}`}
+          onAddToCart={onAddToCart}
+        />
       ))}
     </ul>
   );
 }
 
-function Product({ product }) {
+function Product({ product, onAddToCart }) {
   return (
     <li className="product">
       <img src={product.image} alt={product.title} />
       <div className="product__info">
         <h3 className="product__name">{product.title}</h3>
         <div className="product__price">Rs. {product.price}</div>
-        <button className="product__add-to-cart">Add to cart</button>
+        <button
+          className="product__add-to-cart"
+          onClick={() => onAddToCart(product)}
+        >
+          Add to cart
+        </button>
       </div>
     </li>
   );
